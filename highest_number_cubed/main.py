9463e261de2515e4d44bde75a843a1ab822d98e4
Cyn Armistead <cyn@cynthiaarmistead.com>
2017-02-17 02:20:04 +0000

    Initial commit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,13 @@
 
 
 def highest_number_cubed(limit):
-    pass
+    '''the highest number that can be cubed that's less than the limit'''
+    previous_number = 1
+    
+    while True:
+        current_number = previous_number + 1
+        if current_number ** 3 > limit:
+            return previous_number
+            
+        previous_number = current_number
+    
